d91a53a5a35f5513ac25f135e97306312a836750
Peter Doak <doakpw@ornl.gov>
2017-03-30 19:42:05 -0400

    allAnyone.sh now deals with qos as well

--- allAnyone.sh
+++ allAnyone.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 account=$1
+qos=$2
 account_users=$(getent group cades-$1 | awk -F':' '{print $4}' | sed -e 's/,/ /g')
 
 echo "$account: $account_users"
 echo
-echo "Condo Jobs for $account"
+echo "$qos Jobs for $account"
 # for edde in ${eddes[@]}
 # do
 #     qstat -u ${edde}
@@ -29,16 +30,16 @@ BEGIN {
     group=$9;
     tasks=$4;
     job_account=$11;
-    qos=$5
+    this_qos=$5
     user=$9
     echo "$tasks"
-    if (running ~ /R/ && job_account == account && qos == "condo" && users ~ user  ) {
+    if (running ~ /R/ && job_account == account && this_qos == qos && users ~ user  ) {
             printf("%-10d %3s %5d %12s %12s\\n", $1, user, tasks, $6, $12);  
 	    totaltasks += tasks;
     } 
 }
-END { printf("Current Condo Cores Used By %s: %d\\n", account, totaltasks); }
+END { printf("Current %s Cores Used By %s: %d\\n", qos, account, totaltasks); }
 EOF
 
 printf "%-10s %3s %5s %12s %12s\\n" "job" "uid" "proc" "req wall" "queue time"
-echo "$output" | awk -v users="${account_users[*]}" -v account="$account" "$parseQStats"
+echo "$output" | awk -v users="${account_users[*]}" -v account="$account" -v qos="$qos" "$parseQStats"
