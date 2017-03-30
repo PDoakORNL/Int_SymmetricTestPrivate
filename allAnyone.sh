1fac5936d5e7eacdd64c3025186ae7e6d105e74f
Peter Doak <doakpw@ornl.gov>
2017-03-30 19:58:34 -0400

    an unlimited number of qos can be summed

--- allAnyone.sh
+++ allAnyone.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-account=$1
-qos=$2
+ARGS=("$@")
+account=${ARGS[0]}
+unset ARGS[0]
+qos=(${ARGS[@]})
+
 account_users=$(getent group cades-$1 | awk -F':' '{print $4}' | sed -e 's/,/ /g')
 
 echo "$account: $account_users"
 echo
-echo "$qos Jobs for $account"
+echo "${qos[@]} Jobs for $account"
 # for edde in ${eddes[@]}
 # do
 #     qstat -u ${edde}
@@ -24,6 +27,7 @@ BEGIN {
     split(users, Users, / /)
     running = 0;
     tasks = 0;
+    print qos
 }
 {
     running=$2;
@@ -33,7 +37,7 @@ BEGIN {
     this_qos=$5
     user=$9
     echo "$tasks"
-    if (running ~ /R/ && job_account == account && this_qos == qos && users ~ user  ) {
+    if (running ~ /R/ && job_account == account && qos ~ this_qos && users ~ user  ) {
             printf("%-10d %3s %5d %12s %12s\\n", $1, user, tasks, $6, $12);  
 	    totaltasks += tasks;
     } 
@@ -42,4 +46,4 @@ END { printf("Current %s Cores Used By %s: %d\\n", qos, account, totaltasks); }
 EOF
 
 printf "%-10s %3s %5s %12s %12s\\n" "job" "uid" "proc" "req wall" "queue time"
-echo "$output" | awk -v users="${account_users[*]}" -v account="$account" -v qos="$qos" "$parseQStats"
+echo "$output" | awk -v users="${account_users[*]}" -v account="$account" -v qos="${qos[*]}" "$parseQStats"
